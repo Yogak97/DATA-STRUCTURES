e2f9dac773e58f523d1443559a160ffbc541a66a
Yoga K <kanhedyoganand@gmail.com>
2023-03-08 00:29:08 +0530

    v2 Client server

--- cpp/Socket Programming/FinalClient.cpp
+++ cpp/Socket Programming/FinalClient.cpp
@@ -1,82 +1,64 @@
+/*
+Author : ykanhed
+client program
+*/
+
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
 #include <thread>
+
 #define TCP_PORT 1234
 #define UDP_MULTICAST_GROUP "225.0.0.37"
 #define UDP_MULTICAST_PORT 5678
 #define BUFFER_SIZE 1024
 
-void receiveMulticastMsgUDP(int udp_sock)
+void receiveMulticastMsgUDP(int UDP_SOCKET)
 {
-    // std::cout << "Thread Launched" << std::endl;
     struct sockaddr_in sender_addr;
     socklen_t sender_addr_len = sizeof(sender_addr);
     char buffer[BUFFER_SIZE] = {0};
     while (true)
     {
-        int bytes_received = recvfrom(udp_sock, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
+        int bytes_received = recvfrom(UDP_SOCKET, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
         if (bytes_received == -1)
         {
-            std::cerr << "Failed to receive multicast message" << std::endl;
-            // return -1;
+            std::cout << "EROOR : Failed to receive multicast message" << std::endl;
             break;
         }
-
-        std::cout << "\nReceived multicast message from " << inet_ntoa(sender_addr.sin_addr) << ": " << buffer << std::endl;
+        std::cout << "Received multicast message from " << inet_ntoa(sender_addr.sin_addr) << ": " << buffer << std::endl;
         memset(buffer, 0, BUFFER_SIZE);
     }
-
-    close(udp_sock);
+    close(UDP_SOCKET);
 }
 
 int main(int argc, char const *argv[])
 {
-    // Create a tcp socket
-
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-    {
-        std::cerr << "Failed to create socket." << std::endl;
-        return 1;
-    }
-
-    // Configure the server address and port for tcp
-    sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(TCP_PORT);
-    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0)
-    {
-        std::cerr << "Invalid address." << std::endl;
-        return 1;
-    }
-
     // Create UDP socket
-    int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (udp_sock == -1)
+    int UDP_SOCK = socket(AF_INET, SOCK_DGRAM, 0);
+    if (UDP_SOCK == -1)
     {
-        std::cerr << "Failed to create UDP socket" << std::endl;
+        std::cout << "Failed to create UDP socket" << std::endl;
         return -1;
     }
 
     // allow multiple sockets to use the same PORT number
-    //
     u_int yes = 1;
-    if (setsockopt(udp_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&yes, sizeof(yes)) < 0)
+    if (setsockopt(UDP_SOCK, SOL_SOCKET, SO_REUSEADDR, (char *)&yes, sizeof(yes)) < 0)
     {
-        perror("Reusing ADDR failed");
-        return 1;
+        std::cout << "Reusing ADDR failed" << std::endl;
+        return -1;
     }
 
     // Join multicast group on UDP socket
     struct ip_mreq mreq;
     mreq.imr_multiaddr.s_addr = inet_addr(UDP_MULTICAST_GROUP);
     mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-    if (setsockopt(udp_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&mreq, sizeof(mreq)) == -1)
+    if (setsockopt(UDP_SOCK, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&mreq, sizeof(mreq)) == -1)
     {
-        std::cerr << "Failed to join multicast group" << std::endl;
+        std::cout << "Failed to join multicast group" << std::endl;
         return -1;
     }
 
@@ -85,63 +67,81 @@ int main(int argc, char const *argv[])
     udp_addr.sin_family = AF_INET;
     udp_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     udp_addr.sin_port = htons(UDP_MULTICAST_PORT);
-    if (bind(udp_sock, (struct sockaddr *)&udp_addr, sizeof(udp_addr)) == -1)
+    if (bind(UDP_SOCK, (struct sockaddr *)&udp_addr, sizeof(udp_addr)) == -1)
     {
-        std::cerr << "Failed to bind UDP socket" << std::endl;
+        std::cout << "Failed to bind UDP socket" << std::endl;
         return -1;
     }
 
-    // Connect to the server
-    if (connect(sock, (sockaddr *)&server_address, sizeof(server_address)) == -1)
-    {
-        std::cerr << "Failed to connect to server." << std::endl;
-        return 1;
-    }
-
-    std::thread t1(receiveMulticastMsgUDP, udp_sock);
+    // launch a thread to received udp multicast msgs
+    std::thread t1(receiveMulticastMsgUDP, UDP_SOCK);
     t1.detach();
-
-    // Send a message to the tcp server
 
     while (true)
     {
-        char buffer[BUFFER_SIZE] = {0};
-        // std::this_thread::sleep_for(std::chrono::seconds(1));
-        // sleep(1);
-        // std::cout << "client " << sock << ":";
-        std::cin.getline(buffer, BUFFER_SIZE);
-        if (send(sock, buffer, strlen(buffer), 0) == -1)
+        // Create a tcp socket
+        int TCP_SOCK = socket(AF_INET, SOCK_STREAM, 0);
+        if (TCP_SOCK == -1)
         {
-            std::cerr << "Failed to send message." << std::endl;
+            std::cout << "ERROR : Failed to create tcp socket." << std::endl;
             return 1;
         }
+
+        // Configure the server address and port for tcp
+        sockaddr_in server_address;
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(TCP_PORT);
+        if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0)
+        {
+            std::cout << "Invalid address." << std::endl;
+            return -1;
+        }
+
+        // Connect to the server
+        if (connect(TCP_SOCK, (sockaddr *)&server_address, sizeof(server_address)) == -1)
+        {
+            std::cout << "ERROR : Trying to connect to the server... " << std::endl;
+            close(TCP_SOCK);
+            sleep(1);
+            continue;
+        }
+
+        std::cout << "Success : Connected to the server. " << std::endl;
+
+        // Send a message to the tcp server
+        char buffer[BUFFER_SIZE] = {0};
+        while (true)
+        {
+
+            /*ssize_t receivedBytes = read(TCP_SOCK, buffer, sizeof(buffer) - 1);
+            if (receivedBytes < 0)
+            {
+                std::cout << "ERROR : reading failed from server" << std::endl;
+                break;
+            }
+            else if (receivedBytes == 0)
+            {
+                std::cout << "ERROR : Connection closed by server" << std::endl;
+                break;
+            } */
+            memset(buffer, 0, sizeof(buffer));
+            std::cin.getline(buffer, BUFFER_SIZE);
+            ssize_t sentBytes = send(TCP_SOCK, buffer, strlen(buffer), 0);
+            if (sentBytes == -1)
+            {
+                std::cout << "ERROR : Failed to send message." << std::endl;
+                break;
+            }
+            else if (sentBytes == 0)
+            {
+                std::cout << "ERROR : Connection closed by server" << std::endl;
+                break;
+            }
+        }
+
+        close(TCP_SOCK);
+        sleep(3);
     }
-    // // Receive a response from the server
-    // char buffer[1024] = {0};
-    // if (recv(sock, buffer, 1024, 0) == -1) {
-    //     std::cerr << "Failed to receive message." << std::endl;
-    //     return 1;
-    // }
-    // Receive multicast messages on UDP socket
-
-    // struct sockaddr_in sender_addr;
-    // socklen_t sender_addr_len = sizeof(sender_addr);
-
-    // while (true)
-    // {
-    //     int bytes_received = recvfrom(udp_sock, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
-    //     if (bytes_received == -1)
-    //     {
-    //         std::cerr << "Failed to receive multicast message" << std::endl;
-    //         return -1;
-    //     }
-
-    //     std::cout << "Received multicast message from " << inet_ntoa(sender_addr.sin_addr) << ": " << buffer << std::endl;
-    // }
-
-    // Close the socket
-    // close(udp_sock);
-    close(sock);
 
     return 0;
 }
--- cpp/Socket Programming/FinalServ.cpp
+++ cpp/Socket Programming/FinalServ.cpp
@@ -1,108 +1,136 @@
+/*
+Author : ykanhed
+server program
+*/
+
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 #include <thread>
+
 #define MAX_CLIENTS 10
-#define PORT 1234
+#define TCP_PORT 1234
 #define UDP_PORT 5678
 #define MULTICAST_ADDR "225.0.0.37"
+#define BUFFER_SIZE 1024
 
-// function to handle client connection
-void handle_client(int client_socket)
+// function to handle a new connection from client
+void handle_client(int TCP_CLIENT_SOCKET, struct sockaddr_in client_addr)
 {
+    std::cout << "New connection, sock fd is " << TCP_CLIENT_SOCKET << " ip/port is : " << inet_ntoa(client_addr.sin_addr) << "/" << ntohs(client_addr.sin_port) << std::endl;
+
     char buffer[1024] = {0};
+    // char ACK[] = "ACK";
+    int receivedBytes, sentBytes;
+
+    // multicast message to all connected clients
+    struct sockaddr_in multicast_addr;
+    memset(&multicast_addr, 0, sizeof(multicast_addr));
+    multicast_addr.sin_family = AF_INET;
+    multicast_addr.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
+    multicast_addr.sin_port = htons(UDP_PORT);
+
+    int UDP_SOCK;
+    if ((UDP_SOCK = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        std::cout << "ERROR: Udp socket creation failed " << std::endl;
+    }
+
     while (true)
     {
-        int valread = read(client_socket, buffer, 1024);
-        if (valread == 0)
+
+        /*sentBytes = send(TCP_CLIENT_SOCKET, ACK, sizeof(ACK), 0);
+        if (sentBytes == 0)
         {
-            // client has disconnected
-            printf("Client disconnected, socket fd is %d\n", client_socket);
+            std::cout << "Client disconnected, socket fd is " << TCP_CLIENT_SOCKET << " ip/port : " << inet_ntoa(client_addr.sin_addr) << "/" << ntohs(client_addr.sin_port) << std::endl;
             break;
         }
-        printf("Message received from client (socket fd %d): %s\n", client_socket, buffer);
-
-        // multicast message to all connected clients
-        struct sockaddr_in multicast_addr;
-        memset(&multicast_addr, 0, sizeof(multicast_addr));
-        multicast_addr.sin_family = AF_INET;
-        multicast_addr.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
-        multicast_addr.sin_port = htons(UDP_PORT);
-        int udp_fd;
-        if ((udp_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        else if (sentBytes < 0)
         {
-            perror("socket creation failed");
-            exit(EXIT_FAILURE);
-        }
-        int nbytes = sendto(udp_fd, (const char *)buffer, strlen(buffer), 0, (const struct sockaddr *)&multicast_addr, sizeof(multicast_addr));
-        if (nbytes < 0)
-        {
-            perror("sendto");
-            // return 1;
-        }
-        close(udp_fd);
+            std::cout << "ERROR sending" << std::endl;
+            break;
+        } */
 
-        // send(client_socket, buffer, strlen(buffer), 0); tcp
+        // receive msgs from clients
+        receivedBytes = read(TCP_CLIENT_SOCKET, buffer, BUFFER_SIZE);
+        if (receivedBytes == 0)
+        {
+            std::cout << "Client disconnected, socket fd is " << TCP_CLIENT_SOCKET << " ip/port : " << inet_ntoa(client_addr.sin_addr) << "/" << ntohs(client_addr.sin_port) << std::endl;
+            break;
+        }
+        else if (receivedBytes < 0)
+        {
+            std::cout << "ERROR reading" << std::endl;
+            break;
+        }
+
+        std::cout << "Msg received from " << inet_ntoa(client_addr.sin_addr) << "/" << ntohs(client_addr.sin_port) << ":" << buffer << std::endl;
+
+        // semd udp multicast msg to the clients
+        sentBytes = sendto(UDP_SOCK, (const char *)buffer, strlen(buffer), 0, (const struct sockaddr *)&multicast_addr, sizeof(multicast_addr));
+        if (sentBytes < 0)
+        {
+            std::cout << "ERROR: sending udp multicast msg " << std::endl;
+            break;
+        }
         memset(buffer, 0, sizeof(buffer));
     }
-    close(client_socket);
-    std::cout << "Function finshed" << std::endl;
+
+    close(UDP_SOCK);
+    close(TCP_CLIENT_SOCKET);
 }
 
 int main(int argc, char *argv[])
 {
-    // set up the server socket
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
+    int TCP_LISTEN_SOCK, TCP_CLIENT_SOCK;
+    TCP_LISTEN_SOCK = socket(AF_INET, SOCK_STREAM, 0);
+    if (TCP_LISTEN_SOCK == -1)
+    {
+        std::cout << "ERROR : Failed to create tcp socket." << std::endl;
+        return -1;
+    }
+
     int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if (setsockopt(TCP_LISTEN_SOCK, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+        std::cout << "ERROR : setsockopt" << std::endl;
+        return -1;
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    struct sockaddr_in server_address, client_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(TCP_PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    if (bind(TCP_LISTEN_SOCK, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
     {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, MAX_CLIENTS) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
+        std::cout << "ERROR : bind() failed" << std::endl;
+        return -1;
     }
 
-    // accept incoming connections and handle data in separate threads
+    if (listen(TCP_LISTEN_SOCK, MAX_CLIENTS) < 0)
+    {
+        std::cout << "ERROR : listen() failed" << std::endl;
+        return -1;
+    }
+
+    size_t client_address_length = sizeof(client_address);
+    std::cout << "Server is initiated. Listening for clients! " << std::endl;
     while (true)
     {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        if ((TCP_CLIENT_SOCK = accept(TCP_LISTEN_SOCK, (struct sockaddr *)&client_address, (socklen_t *)&client_address_length)) < 0)
         {
-            perror("accept");
-            exit(EXIT_FAILURE);
+            std::cout << "ERROR : accept() failed" << std::endl;
+            return -1;
         }
-        printf("New connection , socket fd is %d , ip is : %s , port : %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
-        // create new thread to handle client connection
-        std::thread t(handle_client, new_socket);
-        t.detach(); // allow thread to run independently
+        // launch new thread for every client connection
+        std::thread thread_handle_new_client(handle_client, TCP_CLIENT_SOCK, client_address);
+        thread_handle_new_client.detach();
     }
 
     return true;
